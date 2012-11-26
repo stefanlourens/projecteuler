eb5677c0aa8da1dcb265e21bf3c76f2f0ab9e7ac
Stefan Lourens <stefan.lourens@gmail.com>
2012-11-26 21:39:01 +0200

    Fixing comment

--- src/main/scala/problems/Problem_0016.scala
+++ src/main/scala/problems/Problem_0016.scala
@@ -5,7 +5,7 @@ import Character.getNumericValue
 /**
  * http://projecteuler.net/problem=16
  *
- * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
  * What is the sum of the digits of the number 2^1000?
  */
 object Problem_0016 extends Problem {
