243a183d0b48ae4b1eb4d079558120462dddf491
Stefan Lourens <stefan.lourens@gmail.com>
2013-08-13 15:54:24 +0200

    Cleanup

--- src/main/scala/problems/Problem_0006.scala
+++ src/main/scala/problems/Problem_0006.scala
@@ -3,7 +3,6 @@ package problems
 import math.{abs, pow}
 /**
  * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
- *
  */
 object Problem_0006 extends Problem {
 
