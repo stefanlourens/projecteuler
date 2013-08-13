a74d4d9f255dc65aa91dec5db668ef57217481c8
Stefan Lourens <stefan.lourens@gmail.com>
2013-08-13 15:51:55 +0200

    Improve performance

--- src/main/scala/problems/Problem_0010.scala
+++ src/main/scala/problems/Problem_0010.scala
@@ -1,6 +1,6 @@
 package problems
 
-import Problem_0003.isPrime
+import math.sqrt
 
 /**
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
@@ -9,10 +9,13 @@ import Problem_0003.isPrime
  */
 object Problem_0010 extends Problem {
 
-  val primes = for {
-    n <- 2l #:: (3l to Int.MaxValue by 2l).toStream
-    if isPrime(n)
-  } yield n
+  lazy val primes: Stream[Long] = 2l #:: {
+    for {
+      n <- (3l to Int.MaxValue by 2l).toStream
+      maxFactor = sqrt(n)
+      if primes.takeWhile(_ <= maxFactor.intValue()).forall(n % _ != 0)
+    } yield n
+  }
 
   def answer = {
     primes.takeWhile{ _ < 2000000 }.sum
