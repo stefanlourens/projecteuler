b1aece181b8654eefa51f1e79a545275822cba1d
Stefan Lourens <slourens@tagworldwide.com>
2014-01-20 16:29:14 +0200

    Adding prob 28

---
+++ src/main/scala/problems/Problem_0028.scala
@@ -0,0 +1,32 @@
+package problems
+
+/**
+ * http://projecteuler.net/problem=28
+ *
+ * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+ */
+object Problem_0028 extends Problem {
+
+  //Compute the diagonals of the grid
+  val diagonals: Stream[Int] = {
+    var lastNum = 1
+    var addCount = 0
+    var add = 2
+
+    def nextNum(): Stream[Int] = {
+      if (addCount == 4) {
+        addCount = 0
+        add = add + 2
+      }
+
+      addCount = addCount + 1
+      lastNum = lastNum + add
+      lastNum #:: nextNum()
+    }
+
+    1 #:: nextNum()
+  }
+
+  def answer = (diagonals take ((1001 * 2) - 1)).sum
+
+}
