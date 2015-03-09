f32db36a00084463a27b1b3e0e99e185c29eb6fd
Stefan Lourens <slourens@tagworldwide.com>
2015-03-09 17:56:21 +0200

    27

--- src/main/scala/problems/Problem_0027.scala
+++ src/main/scala/problems/Problem_0027.scala
@@ -1,5 +1,7 @@
 package problems
 
+import java.lang.Math.pow
+
 /**
  * http://projecteuler.net/problem=27
  *
@@ -8,8 +10,34 @@ package problems
  */
 object Problem_0027 extends Problem {
 
+  val primes = Problem_0010.primes
 
+  def isPrime(n: Long): Boolean = {
+    primes takeWhile (_ <= n) contains n
+  }
 
-  def answer = 0
+  /**
+   * n² + an + b, where |a| < 1000 and |b| < 1000
+   * @param a
+   * @param b
+   * @return
+   */
+  def formulaPrimeCount(a: Int, b: Int): Int = {
+    (0 to Int.MaxValue takeWhile { n => isPrime(pow(n, 2).toLong + (n * a) + b) } ).size
+  }
+
+  def answer = {
+    val results = for {
+      a <- -999 to 999
+      b <- -999 to 999
+    } yield (a, b, formulaPrimeCount(a, b))
+
+    println(results.size + "total")
+    val (a, b, size) = results.maxBy { case (_, _, count) => count }
+    println("size = " + size)
+    println("a = " + a)
+    println("b = " + b)
+    a * b
+  }
 
 }
